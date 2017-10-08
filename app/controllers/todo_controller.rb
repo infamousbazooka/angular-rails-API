b395c994f725df02c1b385872e6f2abb2c037e82
infamousbazooka <magnusfernandes1295@gmail.com>
2017-10-09 02:36:31 +0530

    All routes complete

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -3,4 +3,30 @@ class TodoController < ApplicationController
     @todos = Todo.all
     render json: @todos
   end
+
+  def create
+    @todo = Todo.create(:title => params[:title], :status => false)
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def update
+    @todo = Todo.find_by(:id => params[:id])
+    @todo.status = !@todo.status
+    @todo.save
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def destroy
+    @todo = Todo.find_by(:id => params[:id])
+    if !@todo
+      output = {'status' => 'not found'}.to_json
+      render :json => output
+      return
+    end
+    @todo.destroy
+    @todos = Todo.all
+    render json: @todos
+  end
 end
